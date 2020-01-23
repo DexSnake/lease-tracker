2363f217645273ad3d5c7c54d1e5b64d62813816
Travis Butler <travisrbutler@gmail.com>
2020-01-22 22:19:03 -0500

    init commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const buyDate = new Date('October 28, 2019 12:00:00')
+const now = new Date()
+const days = Math.round((now - buyDate) / (1000 * 3600 * 24))
+const mpd = 32.87
+const mpy = 12000
+const mileage = Math.round(days * mpd)
+const daysLeft = 365 - days
+const milesLeft = mpy - mileage
+
+
+const input = document.getElementById('input')
+const milesUnder = document.getElementById('milesUnder')
+const milesOver = document.getElementById('milesOver')
+const under = document.getElementById('under')
+const over = document.getElementById('over')
+const error = document.getElementById('error')
+
+
+input.addEventListener('change', () => {
+    inputMiles = input.value
+    const miles = mileage - inputMiles
+    const availableDays = Math.floor(miles / mpd)
+
+    if(isNaN(miles) || inputMiles === "") {
+        error.classList.remove('hide')
+        error.classList.toggle('show')
+        under.classList.add('hide')
+        under.classList.remove('show')
+        over.classList.add('hide')
+        over.classList.remove('show')
+    }else {
+        if (miles >= 0 && availableDays > 0) {
+            milesUnder.innerHTML = miles
+            under.classList.add('show')
+            under.classList.remove('hide')
+            over.classList.add('hide')
+            error.classList.remove('show')
+            error.classList.add('hide')
+            
+        } else {
+            milesOver.innerHTML = Math.abs(miles)
+            over.classList.add('show')
+            over.classList.remove('hide')
+            under.classList.add('hide')
+            error.classList.remove('show')
+            error.classList.add('hide')
+        }
+    }
+})
+
+
